b6326742b1bf3747dfd52b118fc87c2545aac37d
Zirreael <126812503+Zirreael@users.noreply.github.com>
2023-03-25 16:36:59 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -2,3 +2,57 @@
 
 #include <gtest/gtest.h>
 #include "Automata.h"
+// тест включения
+TEST(Automata, test1) {
+  Automata coffee;
+  coffee.on();
+  EXPECT_EQ(coffee.get_state(), "WAIT");
+}
+// тест приготовления напитка
+TEST(Automata, test2) {
+  Automata coffee;
+  coffee.on();
+  coffee.coin(100);
+  coffee.choice(2);
+  EXPECT_EQ(coffee.get_state(), "WAIT");
+}
+// тест отмены
+TEST(Automata, test3) {
+  Automata coffee;
+  coffee.on();
+  coffee.coin(100);
+  coffee.cancel();
+  EXPECT_EQ(coffee.get_state(), "WAIT");
+}
+// тест выключения
+TEST(Automata, test4) {
+  Automata coffee;
+  coffee.on();
+  coffee.coin(100);
+  coffee.cancel();
+  coffee.off();
+  EXPECT_EQ(coffee.get_state(), "OFF");
+}
+// тест "выбран неверный пункт меню"
+TEST(Automata, test5) {
+  Automata coffee;
+  coffee.on();
+  coffee.coin(100);
+  coffee.choice(20);
+  EXPECT_EQ(coffee.get_state(), "WAIT");
+}
+// тест "внесено недостаточно денег"
+TEST(Automata, test6) {
+  Automata coffee;
+  coffee.on();
+  coffee.coin(10);
+  coffee.choice(2);
+  EXPECT_EQ(coffee.get_state(), "WAIT");
+}
+// тест прием денег
+TEST(Automata, test7) {
+  Automata coffee;
+  coffee.on();
+  coffee.coin(100);
+  EXPECT_EQ(coffee.get_state(), "ACCEPT");
+}
