91b1801db609a3d3cec5078196bae4ad51b36e7c
Zirreael <126812503+Zirreael@users.noreply.github.com>
2023-03-25 17:08:21 +0300

    Update Automata.h

--- include/Automata.h
+++ include/Automata.h
@@ -1,36 +1,37 @@
 #pragma once
 #include <iostream>
 #include <string.h>
-#include <windows.h>
-
-using namespace std;
+#include "Windows.h"
 
 class Automata {
 public:
-	enum State {
-		OFF,
-		WAIT,
-		ACCEPT,
-		CHECK,
-		COOK
-	};
+    enum State {
+        OFF,
+        WAIT,
+        ACCEPT,
+        CHECK,
+        COOK
+	    };
+
 private:
-	int cash;
-	int* prices;
-	int count_menu;
-	State state;
-	string* menu;
+    int cash;
+    int* prices;
+    int count_menu;
+    State state;
+    string* menu;
+	
 public:
-	Automata();
-	void on();
-	void off();
-	void coin(int);
-	void get_menu();
-	State get_state();
-	void choice(int);
-	void cancel();
+    Automata();
+    void on();
+    void off();
+    void coin(int);
+    void get_menu();
+    State get_state();
+    void choice(int);
+    void cancel();
+	
 private:
-	void check(int);
-	void cook(int);
-	void finish(int);
+    void check(int);
+    void cook(int);
+    void finish(int);
 };
